910eaafedae13cf36fe5914bba9b97374e366f64
apurv111 <apurv111@users.noreply.github.com>
2015-02-16 18:42:15 +0530

    Update pre-build-script.sh

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,18 +1,1 @@
-sudo apt-get update
-sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev
-sudo apt-get install -y python-software-properties
-sudo -E apt-add-repository -y ppa:aims/sagemath
-sudo apt-get update
-sudo apt-get install -y dvipng
-sudo dpkg --configure -a
-sudo apt-get install -f
-sudo apt-get install -y sagemath-upstream-binary --force-yes
-#if [ "$?" = "1" ]; then
-#    sudo dpkg --configure -a
-#    sudo apt-get install -f
-#    sudo apt-get install -y sagemath-upstream-binary --force-yes
-#    echo "Installation successful!" 1>&2
-#else
-#    echo "F This does not work,Please try your luck again!"
-#fi
-
+(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng; sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
