fe6a3dc6f180189da0c1d504183beae682a73d51
sai krishna <krishna1256@gmail.com>
2015-03-04 13:48:27 +0530

    Update pre-build-script.sh

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$?" -eq "0" ]; then
     (sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
-    if [ "$?" -ne "0" ]; then
+    if [ "$?" -eq "100" ]; then
       (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
     fi
 fi
