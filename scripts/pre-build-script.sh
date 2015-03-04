bf84a43ced1a374a43752930af92b5ecc606749e
soujanya123 <soujanya@vlabs.ac.in>
2015-03-04 13:34:11 +0530

    pre build

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,1 +1,7 @@
-(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng; sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
+#!/bin/sh
+if [ "$?" -eq "0" ]; then
+    (sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
+    if [ "$?" -ne "0" ]; then
+      (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
+    fi
+fi
