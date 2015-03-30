98f32210e86b4f897c7dd106cf405f64c5c7bcfa
sai krishna <krishna1256@gmail.com>
2015-03-30 22:48:33 +0530

    Update pre-build-script.sh

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-echo "conditional check before dvipng...."
-(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
-if [ $? -eq 100 ]; then
-      echo "starting dpkg after dvipng..."
-      (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
-else 
-    echo "else check after dvipng...."
-fi
+(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y sagemath-upstream-binary --force-yes)
+
+#echo "conditional check before dvipng...."
+#(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
+#if [ $? -eq 100 ]; then
+#      echo "starting dpkg after dvipng..."
+#      (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
+#else 
+#    echo "else check after dvipng...."
+#fi
 
