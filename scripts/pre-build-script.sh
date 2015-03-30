5abc108562c9db44aa4a1e5b14199c6433ce1b58
sai krishna <krishna1256@gmail.com>
2015-03-30 22:03:57 +0530

    Update pre-build-script.sh

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ "$?" -eq "0" ]; then
-    (sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
+(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
     if [ "$?" -eq "100" ]; then
+      echo "starting dpkg after dvipng..."
       (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
     fi
-fi
+
