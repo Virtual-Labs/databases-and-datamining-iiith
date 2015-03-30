fb125f517927e4acc3272336ad58857caa57a475
root <root@localhost.localdomain>
2015-03-30 18:51:12 +0000

    logs

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-echo "conditional check before dvipng...."
-(sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng)
-if [ $? -eq 100 ]; then
-      echo "starting dpkg after dvipng..."
-      (sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
-      exit 0
+log_file=/root/log_file.log
+touch $log_file
+echo "conditional check before dvipng...." >> $log_file
+`sudo apt-get update; sudo apt-get install -y make m4 build-essential gcc gfortran libssl-dev python-software-properties expect expect-dev; sudo -E apt-add-repository -y ppa:aims/sagemath; sudo apt-get update; sudo apt-get install -y dvipng`
+
+ret_code=$?
+echo "ret code from dvipng $ret_code" >> $log_file
+
+if [ $ret_code -ne 0 ]; then
+    echo "starting dpkg after dvipng..." >> $log_file
+    `dpkg --configure -a;`
+     echo "dpkg --cinfgure -a" >> $log_file
+    `apt-get install -f; apt-get install -y sagemath-upstream-binary --force-yes`
+    exit $?
 else 
-    echo "else check after dvipng...."
-    exit 0
+    echo "else check after dvipng...." >> $log_file
 fi
-(sudo dpkg --configure -a; sudo apt-get install -f; sudo apt-get install -y sagemath-upstream-binary --force-yes)
+echo "end step.." >> $log_file
 exit 0
